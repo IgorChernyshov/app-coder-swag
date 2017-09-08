2e1292cf898a5e1da2660cad79a21bd1f80930c2
Frostfell <ichernyshov@bk.ru>
2017-09-08 12:51:38 +0300

    Initial commit

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Igor Chernyshov on 08.09.17.
+//  Copyright © 2017 Frostfell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
