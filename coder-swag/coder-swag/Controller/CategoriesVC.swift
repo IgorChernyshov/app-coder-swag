efb389b2e16bd90d08b02a4de4e63207ba5d6fa3
Frostfell <ichernyshov@bk.ru>
2017-09-08 14:02:44 +0300

    Built UI. Started to build custom TableViewController

--- coder-swag/coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/coder-swag/Controller/CategoriesVC.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Igor Chernyshov on 08.09.17.
+//  Copyright © 2017 Frostfell. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
