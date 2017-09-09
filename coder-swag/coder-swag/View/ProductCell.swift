1fbc849c28922aba79d3a88a78509fd9ac759c35
Frostfell <ichernyshov@bk.ru>
2017-09-09 10:03:57 +0300

    Created a custom collection view

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Igor Chernyshov on 09.09.17.
+//  Copyright © 2017 Frostfell. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
